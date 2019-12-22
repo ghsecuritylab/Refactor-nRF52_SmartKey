4e0cb4cb0e08c17016881b0a858fe47d0f14bfa8
NaotoFujihiro <naoto.fujihiro@gmail.com>
2019-12-23 00:21:42 +0900

    Enable to use Sigfox

---
+++ nRF52_Arduino/libraries/unabiz-arduino-master/examples/DemoTest/DemoTest.ino
@@ -0,0 +1,3 @@
+//  DemoTest moved to
+//  DemoTestUnaShieldV1 for UnaShield V1
+//  DemoTestUnaShieldV2S for UnaShield V2S
---
+++ nRF52_Arduino/libraries/unabiz-arduino-master/examples/send-test/send-test.ino
@@ -0,0 +1,4 @@
+/*
+This sample sketch is being updated. Meanwhile please refer to
+ https://github.com/UnaBiz/unabiz-arduino/tree/master/examples/send-light-level
+ */
